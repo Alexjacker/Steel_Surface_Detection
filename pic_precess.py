189d7f94d31dd6c6e354123f537cd0014f9637e4
Alexjacker <2466962821@qq.com>
2022-04-24 12:07:51 +0800

    precess

---
+++ pic_precess.py
@@ -0,0 +1,131 @@
+
+from PIL import Image, ImageEnhance
+import random
+from data_load import *
+# 获取一个批次内样本随机缩放的尺寸
+# 批量读取数据，同一批次内图像的尺寸大小必须是一样的，
+# 不同批次之间的大小是随机的，
+# 由上面定义的get_img_size函数产生
+
+# 随机改变亮暗、对比度和颜色等
+def random_distort(img):
+    # 随机改变亮度
+    def random_brightness(img, lower=0.5, upper=1.5):
+        e = np.random.uniform(lower, upper)
+        return ImageEnhance.Brightness(img).enhance(e)
+    # 随机改变对比度
+    def random_contrast(img, lower=0.5, upper=1.5):
+        e = np.random.uniform(lower, upper)
+        return ImageEnhance.Contrast(img).enhance(e)
+    # 随机改变颜色
+    def random_color(img, lower=0.5, upper=1.5):
+        e = np.random.uniform(lower, upper)
+        return ImageEnhance.Color(img).enhance(e)
+
+    ops = [random_brightness, random_contrast, random_color]
+    np.random.shuffle(ops)
+
+    img = Image.fromarray(img)
+    img = ops[0](img)
+    img = ops[1](img)
+    img = ops[2](img)
+    img = np.asarray(img)
+
+    return img
+# 随机填充
+def random_expand(img,
+                  gtboxes,
+                  max_ratio=4.,
+                  fill=None,
+                  keep_ratio=True,
+                  thresh=0.5):
+    if random.random() > thresh:
+        return img, gtboxes
+
+    if max_ratio < 1.0:
+        return img, gtboxes
+
+    h, w, c = img.shape
+    ratio_x = random.uniform(1, max_ratio)
+    if keep_ratio:
+        ratio_y = ratio_x
+    else:
+        ratio_y = random.uniform(1, max_ratio)
+    oh = int(h * ratio_y)
+    ow = int(w * ratio_x)
+    off_x = random.randint(0, ow - w)
+    off_y = random.randint(0, oh - h)
+
+    out_img = np.zeros((oh, ow, c))
+    if fill and len(fill) == c:
+        for i in range(c):
+            out_img[:, :, i] = fill[i] * 255.0
+
+    out_img[off_y:off_y + h, off_x:off_x + w, :] = img
+    gtboxes[:, 0] = ((gtboxes[:, 0] * w) + off_x) / float(ow)
+    gtboxes[:, 1] = ((gtboxes[:, 1] * h) + off_y) / float(oh)
+    gtboxes[:, 2] = gtboxes[:, 2] / ratio_x
+    gtboxes[:, 3] = gtboxes[:, 3] / ratio_y
+
+    return out_img.astype('uint8'), gtboxes
+# 随机裁剪
+def random_crop(img,
+                boxes,
+                labels,
+                scales=[0.3, 1.0],
+                max_ratio=2.0,
+                constraints=None,
+                max_trial=50):
+    if len(boxes) == 0:
+        return img, boxes
+
+    if not constraints:
+        constraints = [(0.1, 1.0), (0.3, 1.0), (0.5, 1.0), (0.7, 1.0),
+                       (0.9, 1.0), (0.0, 1.0)]
+# 随机缩放
+def random_interp(img, size, interp=None):
+    interp_method = [cv2.INTER_NEAREST,cv2.INTER_LINEAR,cv2.INTER_AREA,cv2.INTER_CUBIC, cv2.INTER_LANCZOS4,]
+    if not interp or interp not in interp_method:
+        interp = interp_method[random.randint(0, len(interp_method) - 1)]
+        h, w, _ = img.shape
+        im_scale_x = size / float(w)
+        im_scale_y = size / float(h)
+        img = cv2.resize(img, None, None, fx=im_scale_x, fy=im_scale_y, interpolation=interp)
+        return img
+def random_flip(img, gtboxes, thresh=0.5):
+    if random.random() > thresh:
+        img = img[:, ::-1, :]
+        gtboxes[:, 0] = 1.0 - gtboxes[:, 0]
+    return img, gtboxes
+def shuffle_gtbox(gtbox, gtlabel):
+    gt = np.concatenate(
+        [gtbox, gtlabel[:, np.newaxis]], axis=1)
+    idx = np.arange(gt.shape[0])
+    np.random.shuffle(idx)
+    gt = gt[idx, :]
+    return gt[:, :4], gt[:, 4]
+def image_augment(img, gtboxes, gtlabels, size, means=None):
+    # 随机改变亮暗、对比度和颜色等
+    img = random_distort(img)
+    # 随机填充
+    img, gtboxes = random_expand(img, gtboxes, fill=means)
+    # 随机裁剪
+    img, gtboxes, gtlabels, = random_crop(img, gtboxes, gtlabels)
+    # 随机缩放
+    img = random_interp(img, size)
+    # 随机翻转
+    img, gtboxes = random_flip(img, gtboxes)
+    # 随机打乱真实框排列顺序
+    gtboxes, gtlabels = shuffle_gtbox(gtboxes, gtlabels)
+
+    return img.astype('float32'), gtboxes.astype('float32'), gtlabels.astype('int32')
+def get_img_data(record, size=640):
+    img, gt_boxes, gt_labels, scales = get_img_data_from_file(record)
+    img, gt_boxes, gt_labels = image_augment(img, gt_boxes, gt_labels, size)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    mean = np.array(mean).reshape((1, 1, -1))
+    std = np.array(std).reshape((1, 1, -1))
+    img = (img / 255.0 - mean) / std
+    img = img.astype('float32').transpose((2, 0, 1))
+    return img
